19cda83b92856cb580c4aeeeb41883490c182493
mateo <matt@email.com>
2024-04-03 02:22:04 +0000

    por fin

--- index.js
+++ index.js
@@ -3,11 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-<<<<<<< HEAD
-  res.send('¡Hola mundo  ahora sera automatico CICD');
-=======
-  res.send('¡Hola mundo a todos! ahora si');
->>>>>>> fd9f589 (final CIDI)
+ res.send('¡Hola mundo a todos! ahora si');
 });
 
 app.listen(3000, () => {
